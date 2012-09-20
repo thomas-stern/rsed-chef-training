7622397fe5f97170320ef57017fbfa85cece077d
thomas-stern <thomas@rightscale.com>
2012-09-20 10:28:34 +0000

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/rsed-test/metadata.rb
@@ -0,0 +1,11 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures rsed-test"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+
+recipe "rsed-test::default","Prints hellow world rsed output"
+
+
